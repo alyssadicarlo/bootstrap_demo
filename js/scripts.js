0f7916c8bd87bef1334992050a8244dda7ebe1fc
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-03 09:56:41 -0700

    added chuck norris quote to modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 'use strict';
 
+fetch(
+    'https://api.chucknorris.io/jokes/random?category=dev'
+).then((response) => {
+    return response.json();
+}).then((data) => {
+    const chuckQuote = document.querySelector('#chuckQuote');
+    chuckQuote.innerText = data.value;
+}).catch((error) => {
+    console.error("ERROR: ", error);
+});
